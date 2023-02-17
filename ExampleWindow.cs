6fecad6de2774169f049d2c9fcf6fe28d7153954
AviDev <105164770+AviDevs@users.noreply.github.com>
2023-02-17 09:14:05 -0400

    Actualizado al español

--- ExampleWindow.cs
+++ ExampleWindow.cs
@@ -1,38 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
-
 public class ExampleWindow : EditorWindow {
 
-	Color color;
+    Color color;
 
-	[MenuItem("Window/Colorizer")]
-	public static void ShowWindow ()
-	{
-		GetWindow<ExampleWindow>("Colorizer");
-	}
+    [MenuItem("Window/Coloriar Objecto")]
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow<ExampleWindow>("Color");
+    }
+    void OnGUI ()
+    {
+        GUILayout.Label("Seleciona el Color del Objeto", EditorStyles.boldLabel);
 
-	void OnGUI ()
-	{
-		GUILayout.Label("Color the selected objects!", EditorStyles.boldLabel);
 
-		color = EditorGUILayout.ColorField("Color", color);
+        color = EditorGUILayout.ColorField("Color", color);
 
-		if (GUILayout.Button("COLORIZE!"))
-		{
-			Colorize();
-		}
-	}
+        if (GUILayout.Button("Coloriar"))
+        {
+            Colorize();
+        }
+    }
 
-	void Colorize ()
-	{
-		foreach (GameObject obj in Selection.gameObjects)
-		{
-			Renderer renderer = obj.GetComponent<Renderer>();
-			if (renderer != null)
-			{
-				renderer.sharedMaterial.color = color;
-			}
-		}
-	}
-
+    void Colorize()
+    {
+        foreach (GameObject obj in Selection.gameObjects)
+        {
+            Renderer renderer = obj.GetComponent<Renderer>();
+            if (renderer != null)
+            {
+                renderer.sharedMaterial.color = color;
+            }
+        }
+    }
 }
